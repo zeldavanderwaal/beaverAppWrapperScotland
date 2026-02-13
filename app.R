8c6a9f2e273c3b0d6f594995a7e93bcd0f897363
zelda van der Waal <zeldavdwaal@gmail.com>
2026-02-13 13:18:23 +0000

    app_password
    in .Renviron
    app_password <- Sys.getenv("BEAVERAPP_PASSWORD")

--- app.R
+++ app.R
@@ -1,21 +1,21 @@
-# This is a Shiny web application to run the real package (beaverApp) from GitHub.
+# This is a Shiny web application to run the real package (beaverApp) from GitHub, along with custom regional data (also on gitHub)
 
 #  the demo data backend 
 options(beaverApp.data.pkg = "beaverAppDataScotland")   #  data package for this deployment 
-Sys.setenv(BEAVERAPP_DATA_PKG = "beaverAppDataScotland", APP_MODE="Scotland") 
+Sys.setenv(BEAVERAPP_DATA_PKG = "beaverAppDataScotland", 
+           APP_MODE="normal",    
+           BEAVERAPP_VERBOSE =FALSE,
+           APP_REGION="Scotland")
+           # BEAVERAPP_PASSWORD= "XXX" 
  
+app_password <- Sys.getenv("BEAVERAPP_PASSWORD") 
  
 options(shiny.maxRequestSize = 7200 * 1024^2)  # just in case, 7200MB uploads
 if (!is.na(Sys.getenv("SHINY_PORT", unset = NA))) {
   future::plan("sequential")
 }
 
-cat("\n>>> launching beaverApp from wrapper (Scotland) >>>\n")
-   
-#library(beaverApp) 
-beaverApp::launch_app(run = FALSE)  
-# return a shiny.appobj for shinyapps.io to run.
-# For shinyapps.io, the last expression in app.R must evaluate to the app object. 
-   
-
+cat("\n>>> launching beaverApp from Scotland wrapper >>>\n")
+    
+beaverApp::launch_app(run = FALSE)   
 
